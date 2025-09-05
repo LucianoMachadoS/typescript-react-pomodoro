b3adfd38f149ebff317df311972e1188045fd57f
Luciano Machado <lucianoo_machado@hotmail.com>
2025-09-05 07:12:30 -0300

    feat: Rest Button

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useInterval } from "../hooks/use-interval";
 import { Button } from "./button";
 import { Timer } from "./timer";
@@ -12,19 +12,48 @@ interface PomodoroTimerProps {
 
 export function PomodoroTimer(props: PomodoroTimerProps) {
   const [mainTime, setMainTime] = useState(props.pomodoroTime);
+  const [timeCounting, setTimeCounting] = useState(false);
+  const [working, setWorking] = useState(false);
+  const [resting, setResting] = useState(false);
+
+  useEffect(() => {
+    if(working) document.body.classList.add('working');
+    if(resting) document.body.classList.remove('working');
+  }, [working]);
 
   useInterval(() => {
     setMainTime(mainTime - 1);
-  }, 1000);
+  }, timeCounting ? 1000 : null);
+
+  const configureWork = () => {
+    setTimeCounting(true);
+    setWorking(true);
+    setResting(false);
+    setMainTime(props.pomodoroTime);
+  };
+
+  const configureRest = (long: boolean) => {
+    setTimeCounting(true);
+    setWorking(false);
+    setResting(true);
+
+    if(long) {
+      setMainTime(props.longRestTime);
+    } else {
+      setMainTime(props.shortRestTime);
+    }
+  };
 
   return (
     <div className="pomodoro">
       <h2>You are: working</h2>
       <Timer mainTime={mainTime} />
       <div className="controls">
-        <Button text="teste" onClick={() => console.log(1)}/>
-        <Button text="teste" onClick={() => console.log(1)}/>
-        <Button text="teste" onClick={() => console.log(1)}/>
+        <Button text="Work" onClick={() => configureWork()}/>
+        <Button text="teste" onClick={() => configureRest(false)}/>
+        <Button
+        className={!working && !resting ? 'hidden' : ''}
+        text={timeCounting ? 'Pause' : 'Play'} onClick={() => setTimeCounting(false)}/>
       </div>
 
       <div className="details">
